246ca8b6671f4678aee1b84cbbdd4177d550646b
satish-rajnale <satishrajnale98@gmail.com>
2021-04-20 23:56:23 +0530

    experiments

---
+++ content/experiments.tsx
@@ -0,0 +1,8 @@
+interface IExperiments {
+    title: string;
+    description: string;
+    link: string;
+    tags: string[];
+    date: string; //TODO: convert to date time
+    location: string;
+  }
