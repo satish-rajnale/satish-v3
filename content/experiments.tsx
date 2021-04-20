25f129cebb3d8de9ecf1dac10815ec4b1069eacc
satish-rajnale <satishrajnale98@gmail.com>
2021-04-21 00:03:36 +0530

    tsconfig

--- content/experiments.tsx
+++
@@ -1,8 +0,0 @@
-interface IExperiments {
-    title: string;
-    description: string;
-    link: string;
-    tags: string[];
-    date: string; //TODO: convert to date time
-    location: string;
-  }
